1a23519ca0e866df035da42a44942d64817a99f9
andrewchen349 <andrewchen349@gmail.com>
2021-04-06 22:22:43 -0400

    added searchbar

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./static")
 
 @app.route("/")
 def hello():
   return render_template("home.html")
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
